f9778aa7ff195b3246e8b3242d2a72c49a8e9bc7
Damon Oehlman <damon.oehlman@gmail.com>
2015-02-19 16:09:31 +1100

    Important documentation

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ var fs = require('fs');
 
   To be completed.
 
+  ## Ready to Use on Any Website
+
+  Because I know how important this script is, it's been browserified to a UMDjs
+  module that can be included using any script tag using the following url:
+
+  ```html
+  <script src="https://cdn.rawgit.com/DamonOehlman/moocow.js/v1.0.0/bundle.js"></script>
+  <script>
+  var newEl = document.createElement('div');
+
+  moocow(document.body);
+  document.body.appendChild(newEl);
+  </script>
+  ```
+
   ## Acknowledgements
 
   - [Mudchute_cow_1.ogg](http://commons.wikimedia.org/wiki/File:Mudchute_cow_1.ogg)
